10c5095d92cdbd2a078c02fff7c9c00097e35267
Your Name <you@example.com>
2023-07-29 15:31:29 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import telebot
+import sqlite3
+
+bot = telebot.TeleBot('6613347539:AAGxjTmDhc-TKBFIKsM5xdthg0sQMqx4U70')
+
+bot.message_handler(commands=['start'])
+def start(message):
+    connect = sqlite3.connect('users.db')
+    cursor = connect.cursor()
+
+    cursor.execute('CREATE TABLE IF NOT EXISTS login_id(id INTEGER);')
+    connect.commit()
+
+    people_id = message.chat.id
+    cursor.execute(f'SELECT id FROM login_id WHERE id = {people_id}')
+    data = cursor.fetchone()
+    if data is None:
+        user_id = [message.chat.id]
+        cursor.execute('INSERT INTO login_id VALUES(?);',user_id)
+        connect.commit()
+    else:
+        bot.send_message(message.chat.id, 'Такой пользователь уже существует')
+
+
+
+bot.message_handler(commands=['delete'])
+def delete(message):
+    connect = sqlite3.connect('users.db')
+    cursor = connect.cursor()
+
+
+    people_id = message.chat.id
+    cursor.execute(f'DELETE FROM login_id WHERE id = {people_id}')
+    connect.commit()
+
+bot.polling()
